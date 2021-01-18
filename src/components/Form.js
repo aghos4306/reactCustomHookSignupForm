6256e1703a12f4c098b964885d28469225031bee
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-01-18 12:33:13 +0200

    css and refactor to Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormSignUp from './FormSignUp';
 import FormSuccess from './FormSuccess';
+import './Form.css';
 
 const Form = () => {
     const [isSubmitted, setIsSubmitted] = React.useState(false);
@@ -9,9 +10,16 @@ const Form = () => {
     }
 
     return (
-        <div>
-            {!isSubmitted ? <FormSignUp submitForm={submitForm} /> : <FormSuccess />}
-        </div>
+        <>
+            <div className="form-container">
+                <span className="close-btn">x</span>
+                <div className="form-content-left">
+                    <img src="img/img-2.svg" className="form-img" alt="formImage" />
+                </div>
+                {!isSubmitted ? <FormSignUp submitForm={submitForm} /> : <FormSuccess />}
+            </div>
+           
+        </>
     )
 }
 
