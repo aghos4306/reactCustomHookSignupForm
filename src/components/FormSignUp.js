965aebc8cb4e74f99a0519ab7ba294fda2bf266d
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-01-18 11:40:26 +0200

    display vaidation error in ui

--- src/components/FormSignUp.js
+++ src/components/FormSignUp.js
@@ -3,7 +3,7 @@ import useForm from './useForm';
 import validate from './ValidateFormInfo';
 
 const FormSignUp = () => {
-    const { handleChange, values, handleSubmit } = useForm(validate);
+    const { handleChange, values, handleSubmit, errors } = useForm(validate);
 
     return (
         <div className="form-content-right">
@@ -14,26 +14,29 @@ const FormSignUp = () => {
                     <input type="text" name="username" className="form-input"  id="username" placeholder="Pls Enter Your Username" 
                     value={values.username}
                     onChange={handleChange}/>
-
                 </div>
+                <p>{errors.username && errors.username}</p>
                 <div className="form-inputs">
                     <label htmlFor="email" className="form-label">Email</label>
                     <input type="email" name="email" className="form-input"  id="email" placeholder="Pls Enter Your Email" 
                     value={values.email}
                     onChange={handleChange}/>
                 </div>
+                <p>{errors.email && errors.email}</p>
                 <div className="form-inputs">
                     <label htmlFor="password" className="form-label">Password</label>
                     <input type="password" name="password" className="form-input"  id="password" placeholder="Pls Enter Your Password" 
                     value={values.password}
                     onChange={handleChange}/>
                 </div>
+                <p>{errors.password && errors.password}</p>
                 <div className="form-inputs">
                     <label htmlFor="password2" className="form-label">Confirm Password</label>
                     <input type="password" name="password2" className="form-input"  id="password2" placeholder="Pls Re-type Password" 
                     value={values.password2}
                     onChange={handleChange}/>
                 </div>
+                <p>{errors.password && errors.password} </p>
                 <button className="form-input-btn" type="submit">Sign Up</button>
                 <span className="form-input-login">
                     Already have an account? Login <a href="#">Here</a>
